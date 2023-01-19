440eb584bdca19f2fe7c21b4fbebe00ef3fc0bed
Thilanka Jayathilaka <thilankas961@gmail.com>
2023-01-19 15:29:10 +0530

    path traversal fixed

--- my-profile.php
+++ my-profile.php
@@ -39,7 +39,10 @@ if (strlen($_SESSION['login']) == 0) {
       exit();
     }
     // Move the uploaded file
-    move_uploaded_file($_FILES["photo"]["tmp_name"], $upload_path . $file_name);
+    $photo = $_FILES["photo"]["tmp_name"];
+    $uppath = $upload_path . $file_name;
+
+    move_uploaded_file($photo, $uppath);
 
     //$ret = mysqli_query($con, "update students set studentName='$studentname',studentPhoto='$photo',cgpa='$cgpa'  where StudentRegno='" . $_SESSION['login'] . "'");
     $stmt = $con->prepare("UPDATE students SET studentName=?, studentPhoto=?, cgpa=? WHERE StudentRegno=?");
@@ -54,10 +57,10 @@ if (strlen($_SESSION['login']) == 0) {
   }
 ?>
 
-  <!DOCTYPE html>
-  <html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-  <head>
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
@@ -66,9 +69,9 @@ if (strlen($_SESSION['login']) == 0) {
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet" />
-  </head>
+</head>
 
-  <body>
+<body>
     <?php include('includes/header.php'); ?>
     <!-- LOGO HEADER END-->
     <?php if ($_SESSION['login'] != "") {
@@ -77,76 +80,83 @@ if (strlen($_SESSION['login']) == 0) {
     ?>
     <!-- MENU SECTION END-->
     <div class="content-wrapper">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h1 class="page-head-line">Student Registration </h1>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-3"></div>
-          <div class="col-md-6">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                Student Registration
-              </div>
-              <font color="green" align="center">
-                <?php echo htmlentities($_SESSION['msg']); ?><?php echo htmlentities($_SESSION['msg'] = ""); ?>
-              </font>
-              <?php $sql = mysqli_query($con, "select * from students where StudentRegno='" . $_SESSION['login'] . "'");
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="page-head-line">Student Registration </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Student Registration
+                        </div>
+                        <font color="green" align="center">
+                            <?php echo htmlentities($_SESSION['msg']); ?><?php echo htmlentities($_SESSION['msg'] = ""); ?>
+                        </font>
+                        <?php $sql = mysqli_query($con, "select * from students where StudentRegno='" . $_SESSION['login'] . "'");
               $cnt = 1;
               while ($row = mysqli_fetch_array($sql)) { ?>
 
-                <div class="panel-body">
-                  <form name="dept" method="post" enctype="multipart/form-data">
-                    <div class="form-group">
-                      <label for="studentname">Student Name </label>
-                      <input type="text" class="form-control" id="studentname" name="studentname" value="<?php echo htmlentities($row['studentName']); ?>" />
+                        <div class="panel-body">
+                            <form name="dept" method="post" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <label for="studentname">Student Name </label>
+                                    <input type="text" class="form-control" id="studentname" name="studentname"
+                                        value="<?php echo htmlentities($row['studentName']); ?>" />
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="studentregno">Student Reg No </label>
+                                    <input type="text" class="form-control" id="studentregno" name="studentregno"
+                                        value="<?php echo htmlentities($row['StudentRegno']); ?>"
+                                        placeholder="Student Reg no" readonly />
+
+                                </div>
+
+
+
+                                <div class="form-group">
+                                    <label for="Pincode">Pincode </label>
+                                    <input type="text" class="form-control" id="Pincode" name="Pincode" readonly
+                                        value="<?php echo htmlentities($row['pincode']); ?>" required />
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="CGPA">CGPA </label>
+                                    <input type="text" class="form-control" id="cgpa" name="cgpa"
+                                        value="<?php echo htmlentities($row['cgpa']); ?>" required />
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label for="Pincode">Student Photo </label>
+                                    <?php if ($row['studentPhoto'] == "") { ?>
+                                    <img src="studentphoto/noimage.png" width="200" height="200"><?php } else { ?>
+                                    <img src="studentphoto/<?php echo htmlentities($row['studentPhoto']); ?>"
+                                        width="200" height="200">
+                                    <?php } ?>
+                                </div>
+                                <div class="form-group">
+                                    <label for="Pincode">Upload New Photo </label>
+                                    <input type="file" class="form-control" id="photo" name="photo"
+                                        value="<?php echo htmlentities($row['studentPhoto']); ?>" />
+                                </div>
+
+
+                                <?php } ?>
+
+                                <button type="submit" name="submit" id="submit" class="btn btn-default">Update</button>
+                            </form>
+                        </div>
                     </div>
-
-                    <div class="form-group">
-                      <label for="studentregno">Student Reg No </label>
-                      <input type="text" class="form-control" id="studentregno" name="studentregno" value="<?php echo htmlentities($row['StudentRegno']); ?>" placeholder="Student Reg no" readonly />
-
-                    </div>
-
-
-
-                    <div class="form-group">
-                      <label for="Pincode">Pincode </label>
-                      <input type="text" class="form-control" id="Pincode" name="Pincode" readonly value="<?php echo htmlentities($row['pincode']); ?>" required />
-                    </div>
-
-                    <div class="form-group">
-                      <label for="CGPA">CGPA </label>
-                      <input type="text" class="form-control" id="cgpa" name="cgpa" value="<?php echo htmlentities($row['cgpa']); ?>" required />
-                    </div>
-
-
-                    <div class="form-group">
-                      <label for="Pincode">Student Photo </label>
-                      <?php if ($row['studentPhoto'] == "") { ?>
-                        <img src="studentphoto/noimage.png" width="200" height="200"><?php } else { ?>
-                        <img src="studentphoto/<?php echo htmlentities($row['studentPhoto']); ?>" width="200" height="200">
-                      <?php } ?>
-                    </div>
-                    <div class="form-group">
-                      <label for="Pincode">Upload New Photo </label>
-                      <input type="file" class="form-control" id="photo" name="photo" value="<?php echo htmlentities($row['studentPhoto']); ?>" />
-                    </div>
-
-
-                  <?php } ?>
-
-                  <button type="submit" name="submit" id="submit" class="btn btn-default">Update</button>
-                  </form>
                 </div>
+
             </div>
-          </div>
 
         </div>
-
-      </div>
 
 
 
@@ -159,7 +169,7 @@ if (strlen($_SESSION['login']) == 0) {
     <script src="assets/js/bootstrap.js"></script>
 
 
-  </body>
+</body>
 
-  </html>
+</html>
 <?php } ?>
